143170e4269a3e526eb02285dc52572c9092d996
LCRoque <lucas.roque@sptech.school>
2024-09-23 12:30:09 -0300

    Subindo alterações do banco e do site

--- BANCO DE DADOS/BancoDeDados-RunGuard.sql
+++ BANCO DE DADOS/BancoDeDados-RunGuard.sql
@@ -3,46 +3,49 @@ drop database if exists runguard;
 create database runguard;
 use runguard;
 
-
-
 -- CRIAÇÃO DE TABELAS
 create table empresa (
 idEmpresa int primary key auto_increment,
-nome_empresa varchar(45)
+razaoSocial varchar(45),
+cnpj char(18)
 );
 
 create table usuario (
 idUsuario int primary key auto_increment,
-nome_usuario varchar(45),
-email_usuario varchar(45) unique,
-senha_usuario varchar(45),
-codigo int,
-fkEmpresa_usuario int,
-    constraint fkEmpresaUsuario foreign key (fkEmpresa_usuario) references empresa (idEmpresa)
+nome varchar(45),
+email varchar(45) unique,
+senha varchar(45),
+cpf varchar(14),
+cargo varchar(45),
+fkEmpresa int,
+    constraint fkEmpresaUsuario foreign key (fkEmpresa) references empresa (idEmpresa)
 );
 
 select * from usuario;
 
 create table equipamento (
 idEquipamento int primary key auto_increment,
-nome_equipamento varchar(45),
-fkEmpresa_equipamento int,
-    constraint fkEmpresaEquipamento foreign key (fkEmpresa_equipamento) references empresa (idEmpresa)
+nomeEquipamento varchar(45),
+cpuVersao varchar(45),
+memoriaRam varchar(45),
+sistemaOperacional varchar(45),
+fkEmpresa int,
+    constraint fkEmpresaEquipamento foreign key (fkEmpresa) references empresa (idEmpresa)
 );
 
-create table dados (
-idDados int primary key auto_increment,
-cpu_porcent decimal(20,1),
-memoria_porcent decimal(20,1),
-memoria_usada decimal(20,1),
+create table dado (
+idDado int primary key auto_increment,
+cpuPercent double,
+memoriaPercent double,
+memoriaUsada double,
 dtHora datetime default current_timestamp,
 fkEquipamento int,
     constraint fkEquipamentoDados foreign key (fkEquipamento) references equipamento (idEquipamento)
 );
 
 insert into empresa values
-(default, 'Uber'),
-(default, '99 Táxi');
+(default, 'Uber', '123123123'),
+(default, '99 Táxi', '18321371');
 
 insert into equipamento values
 (default, 'M1', 1),
@@ -50,24 +53,24 @@ insert into equipamento values
 (default, 'M3', 1),
 (default, 'M4', 1);
 
-select * from dados;
+select * from dado;
 
 CREATE VIEW Monitoramento as
 SELECT 
-    d.idDados AS ID,
-    CONCAT(d.cpu_porcent, "%") AS "Porcentagem CPU",
-    CONCAT(d.memoria_porcent, "%") AS "Porcentagem Memoria",
-    CONCAT(d.memoria_usada, "GB") AS "Memoria usada",
-    CONCAT(d.disco_porcent_usado, "%") AS "Porcentagem de Disco Usada",
+    d.idDado AS ID,
+    CONCAT(d.cpuPercent, "%") AS "Porcentagem CPU",
+    CONCAT(d.memoriaPercent, "%") AS "Porcentagem Memoria",
+    CONCAT(d.memoriaUsada, "GB") AS "Memoria usada",
     d.dtHora AS "Data",
-    e.nome_equipamento AS Equipamento
+    e.nomeEquipamento AS Equipamento
 FROM 
-    dados as d
+    dado as d
 JOIN 
     equipamento as e ON d.fkEquipamento = e.idEquipamento;
 
 SELECT * FROM Monitoramento;
 
-SELECT * FROM dados;
+SELECT * FROM dado;
+select * from empresa;
 
 select * from usuario;
