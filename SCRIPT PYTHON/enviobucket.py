1d2e58501c5db9186a5e7687752d02effe95a4f7
DiegoCampos2507 <diego.campos@sptech.school>
2024-10-09 10:51:43 -0300

    Atualizando script python (Inserção no banco e envio bucket simultaneamente)

--- SCRIPT PYTHON/enviobucket.py
+++ SCRIPT PYTHON/enviobucket.py
@@ -1,8 +1,10 @@
 import psutil
 import time
+import mysql.connector
 import pandas as pd
 import boto3
-from socket import gethostname
+import platform
+import socket
 
 run = True
 
@@ -14,16 +16,37 @@ aws_secret_access_key='',
 aws_session_token='',
 region_name='')
 
+# Criação da conexão do Banco de Dados
+mydb = mysql.connector.connect(
+  host="localhost",
+  user="root",
+  password="Ubatuba0815",
+  database="runguard",
+  port=3306 
+)
+
+mycursor = mydb.cursor()
+
+sistemaOperacional = platform.system()
+run = True
+dados = []
+
+# Função para converter os valores de Bytes para Gigabytes
 def byte_para_gb(byte):
     return byte / (1024 ** 3)
 
-tempo = int(input("Digite o intervalo de tempo que você quer entre os cadastros (em segundos): "))
+nomeMaquina = socket.gethostname()
+memoria_total = psutil.virtual_memory().total
+cpu_versao = platform.processor()
 
-dados = []
-nome = gethostname()
+tempo = int(input("Digite o intevalo de tempo que você quer entre os cadastros: "))
+
+fkEmpresa = input("Digite a o código de sua empresa: ")
+
 
 while run:
     print('=======================')
+    print("Pressione Ctrl+C para interromper o script")
 
     # Obtém as informações da CPU e da memória
     cpu = psutil.cpu_percent()  # Porcentagem de uso da CPU
@@ -33,13 +56,56 @@ while run:
     memoria_usada_formatada = f'{memoria_usada:.1f}'  # Formata o número
 
     # Imprime as informações no terminal
-    print(f'Nome salvo: {nome}')
+    print(f'Nome salvo: {nomeMaquina}')
     print(f'A CPU está em {cpu} %')
     print(f'A memória está em {memoria.percent} %')
 
+    #Select para verificação da inserção do equipamento
+    instrucaoVerEquipamento = "SELECT * FROM equipamento WHERE nomeEquipamento = %s" 
+    mycursor.execute(instrucaoVerEquipamento, ([nomeMaquina]))
+
+    #Função para utilizar o resultado do mycursor, se não da erro de unread result
+    for row in mycursor:  
+        print(f"Máquina selecionada: {row}")
+
+    #Função para verificar (apartir do select de cima) se já existe um equipamento com esse nome para fazer inserção automática dele
+    if mycursor.rowcount < 1: 
+        sql = "INSERT INTO equipamento VALUES (default, %s, %s, %s, %s, %s)"
+        values = (nomeMaquina,cpu_versao, memoria_total, sistemaOperacional, fkEmpresa)
+        mycursor.execute(sql, values) 
+        mydb.commit()
+
+    instrucaoID = "SELECT idEquipamento FROM equipamento WHERE nomeEquipamento LIKE %s"
+    valuesID = ([nomeMaquina])
+    mycursor.execute(instrucaoID, valuesID)
+    idEquipamento_tupla = mycursor.fetchone()
+
+    #Seleção do id selecionado
+    idEquipamento = idEquipamento_tupla[0]
+
+    # Imprime as informações no terminal para visualização
+    print(f"""A CPU está em {cpu} %
+A memória está em {memoria.percent} %
+Total de memória usada: {memoria_usada_formatada} GB""")
+
+    # Faz as inserções no banco de dados passando os componentes
+    sql = "INSERT INTO dados (idDado, cpuPercent, memoriaPercent, memoriaUsada, dtHora, fkEquipamento) VALUES (default, %s, %s, %s, default, %s)"
+    val = (cpu,memoria.percent,memoria_usada_formatada, idEquipamento)
+
+    mycursor.execute(sql,val)
+    mydb.commit()
+
+    sql = "SELECT * FROM dados"
+
+    mycursor.execute(sql)
+    dados_coletados = mycursor.fetchall()
+    mydb.commit()
+
+    print(dados_coletados)
+
     # Adiciona os dados à lista
     dados.append({
-        'nome': nome,
+        'nome': nomeMaquina,
         'cpu': cpu,
         'memoria': memoria.percent
     })
