79f63142c8046cb853d7e7060ce08f1b1122d973
DiegoCampos2507 <diego.campos@sptech.school>
2024-11-05 10:46:01 -0300

    finalizando dash (primeira página)

--- SITE/API-LIMPA/web-data-viz/src/controllers/dashboardDiegoController.js
+++ SITE/API-LIMPA/web-data-viz/src/controllers/dashboardDiegoController.js
@@ -1,12 +1,26 @@
 var dashboardDiegoModel = require("../models/dashboardDiegoModel.js");
 
 function atualizarGrafico(req, res) {
-    dashboardDiegoModel.atualizarGrafico()
+    var mes1 = req.params.minValue
+    var mes2 = req.params.maxValue
+
+    dashboardDiegoModel.atualizarGrafico(mes1, mes2)
     .then(function (resultado) {
         res.status(200).json(resultado);
     })
 }
 
+function coletarDados(req, res) {
+    var mes1 = req.params.minValue
+    var mes2 = req.params.maxValue
+
+    dashboardDiegoModel.coletarDados(mes1, mes2)
+    .then(function (resultado) {
+        res.status(200).json(resultado)
+    })
+}
+
 module.exports = {
     atualizarGrafico,
+    coletarDados,
 }
--- SITE/API-LIMPA/web-data-viz/src/routes/dashboard-Diego.js
+++ SITE/API-LIMPA/web-data-viz/src/routes/dashboard-Diego.js
@@ -3,8 +3,12 @@ var router = express.Router();
 
 var dashboardDiegoController = require("../controllers/dashboardDiegoController.js");
 
-router.get('/atualizarGrafico', function(req,res) {
+router.get(`/atualizarGrafico/:minValue/:maxValue`, function(req,res) {
     dashboardDiegoController.atualizarGrafico(req,res);
+});
+
+router.get(`/coletarDados/:minValue/:maxValue`, function(req,res) {
+    dashboardDiegoController.coletarDados(req,res);
 });
 
 module.exports = router;
