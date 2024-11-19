09c9309470aeecb27fe4738f437d1b3c0bd2e3fe
Alexandre <alexandre.nichiojr@sptech.school>
2024-11-18 22:59:40 -0300

    ajuste script de python e dashs

--- SCRIPT PYTHON/enviobucket.py
+++ SCRIPT PYTHON/enviobucket.py
@@ -5,7 +5,7 @@ import pandas as pd
 import boto3
 import platform
 import socket
-
+import speedtest
 
 run = True
 
@@ -16,7 +16,6 @@ aws_access_key_id='',
 aws_secret_access_key='',
 aws_session_token='',
 region_name='')
-
 
 # Criação da conexão do Banco de Dados
 mydb = mysql.connector.connect(
@@ -30,7 +29,6 @@ mydb = mysql.connector.connect(
 mycursor = mydb.cursor()
 
 sistemaOperacional = platform.system()
-run = True
 dados = []
 
 # Função para converter os valores de Bytes para Gigabytes
@@ -45,11 +43,15 @@ tempo = int(input("Digite o intevalo de tempo que você quer entre os cadastros:
 
 fkEmpresa = input("Digite a o código de sua empresa: ")
 
+def get_ping():
+    st = speedtest.Speedtest()
+    st.get_best_server()
+    ping = st.results.ping
+    return ping
 
 while run:
     print('=======================')
     print("Pressione Ctrl+C para interromper o script")
-
 
     # Obtém as informações da CPU e da memória
     cpu = psutil.cpu_percent()  # Porcentagem de uso da CPU
@@ -63,27 +65,29 @@ while run:
     pacotes_recebidos = net_io.packets_recv
     memoria_usada = byte_para_gb(memoria.used)  # Converte bytes para Gigabytes
     memoria_usada_formatada = f'{memoria_usada:.1f}'  # Formata o número
-    idEquipamento =1
-    # Imprime as informações no terminal
 
+    # Captura o ping
+    ping = get_ping()
+
+    # Imprime as informações no terminal
     print(f'Nome salvo: {nomeMaquina}')
     print(f'A CPU está em {cpu} %')
     print(f'A memória está em {memoria.percent} %')
     print(f'O envio de bytes está em {bytes_enviados:.2f} bytes')
     print(f'O recebimento de bytes está em {bytes_recebidos:.2f} bytes')
-    print(f'O envio de pacotes está em {pacotes_enviados:.2f} bytes')
-    print(f'O recebimento de pacotes está em {pacotes_recebidos:.2f} bytes')
-    
+    print(f'Quantidade de pacotes enviados em  {pacotes_enviados:.2f} ')
+    print(f'Quantidade de pacotes recebidos em {pacotes_recebidos:.2f} ')
+    print(f'Ping: {ping} ms')
 
-    #Select para verificação da inserção do equipamento
+    # Select para verificação da inserção do equipamento
     instrucaoVerEquipamento = "SELECT * FROM equipamento WHERE nomeEquipamento = %s" 
     mycursor.execute(instrucaoVerEquipamento, ([nomeMaquina]))
 
-    #Função para utilizar o resultado do mycursor, se não da erro de unread result
+    # Função para utilizar o resultado do mycursor, se não da erro de unread result
     for row in mycursor:  
         print(f"Máquina selecionada: {row}")
 
-    #Função para verificar (apartir do select de cima) se já existe um equipamento com esse nome para fazer inserção automática dele
+    # Função para verificar (apartir do select de cima) se já existe um equipamento com esse nome para fazer inserção automática dele
     if mycursor.rowcount < 1: 
         sql = "INSERT INTO equipamento VALUES (default, %s, %s, %s, %s, %s)"
         values = (nomeMaquina,cpu_versao, memoria_total, sistemaOperacional, fkEmpresa)
@@ -95,9 +99,8 @@ while run:
     mycursor.execute(instrucaoID, valuesID)
     idEquipamento_tupla = mycursor.fetchone()
 
-    #Seleção do id selecionado
+    # Seleção do id selecionado
     idEquipamento = idEquipamento_tupla[0]
-   
 
     # Imprime as informações no terminal para visualização
     print(f"""A CPU está em {cpu} %
@@ -105,16 +108,15 @@ A memória está em {memoria.percent} %
 Total de memória usada: {memoria_usada_formatada} GB %
 O envio de bytes está em {bytes_enviados/ 1024 :.2f} bytes %
 O recebimento de bytes está em {bytes_recebidos/ 1024 :.2f} bytes %
-O envio de pacotes está em {pacotes_enviados/ 1024 :.2f} bytes %
-O recebimento de pacotes está em {pacotes_recebidos/ 1024 :.2f} bytes""")
-    
-
+Quantidade de pacotes enviados em {pacotes_enviados}  %
+Quantidade de pacotes recebidos em {pacotes_recebidos}  %
+Ping: {ping} ms""")
 
     # Faz as inserções no banco de dados passando os componentes
-    sql = "INSERT INTO dados (idDado, cpuPercent, memoriaPercent, memoriaUsada,bytes_recebidos ,bytes_enviados ,pacotes_recebidos ,pacotes_enviados , dtHora, fkEquipamento) VALUES (default, %s, %s, %s,%s,%s,%s,%s, default, %s)"
-    val = (cpu,memoria.percent,memoria_usada_formatada,bytes_recebidos,bytes_enviados,pacotes_recebidos,pacotes_enviados,idEquipamento)
+    sql = "INSERT INTO dados (idDado, cpuPercent, memoriaPercent, memoriaUsada, bytes_recebidos, bytes_enviados, pacotes_recebidos, pacotes_enviados, ping, dtHora, fkEquipamento) VALUES (default, %s, %s, %s, %s, %s, %s, %s, %s, default, %s)"
+    val = (cpu, memoria.percent, memoria_usada_formatada, bytes_recebidos, bytes_enviados, pacotes_recebidos, pacotes_enviados, ping, idEquipamento)
 
-    mycursor.execute(sql,val)
+    mycursor.execute(sql, val)
     mydb.commit()
 
     sql = "SELECT * FROM dados"
@@ -132,8 +134,9 @@ O recebimento de pacotes está em {pacotes_recebidos/ 1024 :.2f} bytes""")
         'memoria': memoria.percent,
         'dados_enviados': bytes_enviados,
         'dados_recebidos': bytes_recebidos,
-        'pacotes_enviados':pacotes_enviados,
-        'pacotes_recebidos':pacotes_recebidos       
+        'pacotes_enviados': pacotes_enviados,
+        'pacotes_recebidos': pacotes_recebidos,
+        'ping': ping       
     })
 
     # Cria um DataFrame a partir da lista de dados
@@ -144,12 +147,10 @@ O recebimento de pacotes está em {pacotes_recebidos/ 1024 :.2f} bytes""")
     # Salva o DataFrame em um arquivo json
     df.to_json(caminho_arquivo, orient='records', lines=False)
 
-
     print('Dados salvos com sucesso!')
 
     nome_bucket = "raw-stocks-alexandre"
     chave_bucket = "dados.json"
-
 
     # Faz upload de um arquivo para um bucket específico com um nome específico para o arquivo
     s3_client.upload_file(caminho_arquivo, nome_bucket, chave_bucket)
