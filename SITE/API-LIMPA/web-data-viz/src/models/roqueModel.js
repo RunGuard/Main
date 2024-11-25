6dbfe101ad2382ecce584f570421d70f78727c9f
LCRoque <lucas.roque@sptech.school>
2024-11-25 11:21:10 -0300

    Fix: Filtro no html
    Atualizei no script o filtro de seleção do meu fetch na linha 7

--- SITE/API-LIMPA/web-data-viz/src/models/roqueModel.js
+++ SITE/API-LIMPA/web-data-viz/src/models/roqueModel.js
@@ -32,31 +32,38 @@ async function buscarMedidaModel(fkEquipamento) {
 
 async function buscarMedidasPorPeriodoModel(fkEquipamento, mes, semana) {
     let query = `
-        SELECT 
-            nomeEquipamento,
-            dtHora AS momento, 
-            cpuPercent AS uso_cpu, 
-            memoriaPercent AS uso_memoria, 
-            desvio_padrao_cpu, 
-            desvio_padrao_memoria, 
-            taxa_io_disco AS uso_io_total, 
-            downtime AS tempo_downtime,
-            indice_estabilidade, 
-            eficiencia_io_cpu, 
-            taxa_recuperacao
-        FROM dado 
-        JOIN equipamento ON fkEquipamento = idEquipamento
-        WHERE fkEquipamento = ${fkEquipamento} 
-          AND MONTH(dtHora) = ${mes}
+    SELECT 
+        nomeEquipamento,
+        dtHora AS momento, 
+        cpuPercent AS uso_cpu, 
+        memoriaPercent AS uso_memoria, 
+        desvio_padrao_cpu, 
+        desvio_padrao_memoria, 
+        taxa_io_disco AS uso_io_total, 
+        downtime AS tempo_downtime,
+        indice_estabilidade, 
+        eficiencia_io_cpu, 
+        taxa_recuperacao
+    FROM dado 
+    JOIN equipamento ON fkEquipamento = idEquipamento
+    WHERE fkEquipamento = ${fkEquipamento} 
+      AND MONTH(dtHora) = ${mes}
+`;
+
+if (semana) {
+    // Verifica se a semana é entre 1 e 4
+    query += `
+      AND CEIL((DAY(dtHora) + DAYOFWEEK(DATE_SUB(dtHora, INTERVAL DAYOFMONTH(dtHora) - 1 DAY)) - 2) / 7) = ${semana}
     `;
+} else {
+    // Filtro para as semanas de 1 a 4, caso o parâmetro semana não seja passado
+    query += `
+      AND CEIL((DAY(dtHora) + DAYOFWEEK(DATE_SUB(dtHora, INTERVAL DAYOFMONTH(dtHora) - 1 DAY)) - 2) / 7) BETWEEN 1 AND 4
+    `;
+}
 
-    if (semana) {
-        query += `
-          AND WEEK(dtHora, 1) = ${semana}
-        `;
-    }
+query += ` ORDER BY dtHora DESC LIMIT 5;`;
 
-    query += ` ORDER BY dtHora DESC LIMIT 5;`;
 
     try {
         const resultados = await database.executar(query);
