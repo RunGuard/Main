625447e280677fd6fa4c12ba0f8fa807a5b1543c
LCRoque <lucas.roque@sptech.school>
2024-12-02 23:03:17 -0300

    New: Projeto Individual finalizado
    Alterações realizada com sucesso

--- SITE/API-LIMPA/web-data-viz/src/models/roqueModel.js
+++ SITE/API-LIMPA/web-data-viz/src/models/roqueModel.js
@@ -32,35 +32,34 @@ async function buscarMedidaModel(fkEquipamento) {
 
 async function buscarMedidasPorPeriodoModel(fkEquipamento, mes, semana = null) {
     let query = `
-        SELECT 
-            nomeEquipamento,
-            dtHora AS momento, 
-            cpuPercent AS uso_cpu, 
-            memoriaPercent AS uso_memoria, 
-            desvio_padrao_cpu, 
-            desvio_padrao_memoria, 
-            taxa_io_disco AS uso_io_total, 
-            downtime AS tempo_downtime,
-            indice_estabilidade, 
-            eficiencia_io_cpu, 
-            taxa_recuperacao
-        FROM dado 
-        JOIN equipamento ON fkEquipamento = idEquipamento
-        WHERE fkEquipamento = ${fkEquipamento} 
-          AND MONTH(dtHora) = ${mes}
-    `;
+    SELECT 
+        nomeEquipamento,
+        WEEK(dtHora) AS semana,
+        MONTH(dtHora) AS mes,
+        AVG(cpuPercent) AS media_uso_cpu,
+        AVG(memoriaPercent) AS media_uso_memoria,
+        AVG(desvio_padrao_cpu) AS media_desvio_padrao_cpu,
+        AVG(desvio_padrao_memoria) AS media_desvio_padrao_memoria,
+        AVG(taxa_io_disco) AS media_uso_io_total,
+        AVG(downtime) AS media_tempo_downtime,
+        AVG(indice_estabilidade) AS media_indice_estabilidade,
+        AVG(eficiencia_io_cpu) AS media_eficiencia_io_cpu,
+        AVG(taxa_recuperacao) AS media_taxa_recuperacao
+    FROM dado
+    JOIN equipamento ON fkEquipamento = idEquipamento
+    WHERE fkEquipamento = ${fkEquipamento}
+`;
 
-    if (semana) {
-        query += `
-          AND semana = ${semana}
-        `;
-    } else {
-        query += `
-          AND semana BETWEEN 1 AND 4
-        `;
-    }
+if (mes) {
+    query += ` AND MONTH(dtHora) = ${mes} `;
+}
 
-    query += ` ORDER BY dtHora DESC LIMIT 5;`;
+if (semana) {
+    query += ` AND WEEK(dtHora) = ${semana} `;
+}
+
+query += ` GROUP BY nomeEquipamento, semana, mes ORDER BY mes, semana DESC LIMIT 5; `;
+
 
     try {
         const resultados = await database.executar(query);
